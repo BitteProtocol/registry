99aab9b8d9418d8bf43dae5df1fd2a88586a6202
Guilherme Allen <allen98.gp@gmail.com>
2025-04-10 14:33:18 +0100

    Use workaround prisma plugin?

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,6 @@
 import type { NextConfig } from "next";
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const { PrismaPlugin } = require('@prisma/nextjs-monorepo-workaround-plugin')
 
 const nextConfig: NextConfig = {
   images: {
@@ -12,6 +14,12 @@ const nextConfig: NextConfig = {
     ],
   },
   outputFileTracingRoot: __dirname,
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins.push(new PrismaPlugin());
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
